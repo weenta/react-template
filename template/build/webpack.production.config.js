f5e74b6c59ae4735321bf041d7f6176fd66fffe0
weenta <weenta@qq.com>
2018-02-17 07:17:18 +0800

    优化webpack配置

--- template/build/webpack.production.config.js
+++ template/build/webpack.production.config.js
@@ -49,7 +49,7 @@ module.exports = {
                 }
             },
             {
-                test: /\.(png|woff|woff2|svg|ttf|eot)($|\?)/,
+                test: /\.(woff|woff2|svg|ttf|eot)($|\?)/,
                 use: {
                     loader: 'url-loader',
                     options: {
